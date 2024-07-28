535d5f67ed1e3478eebecee1a374ff0b31e8af0f
MyungJiwoo <1206jiwoo@gmail.com>
2024-07-28 23:27:53 +0900

    [#2] 메인 페이지 디자인

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { Container } from "../styles/MainPageStyled";
+import PostCard from "../components/PostCard";
 
 const MainPage = () => {
-    return <Container></Container>;
+    return (
+        <Container>
+            <p className="title">Lastest</p>
+            <p className="subTitle">가장 최근에 작성한 글입니다.</p>
+
+            <div className="postBox">{/* <PostCard /> */}</div>
+        </Container>
+    );
 };
 export default MainPage;
--- src/styles/MainPageStyled.tsx
+++ src/styles/MainPageStyled.tsx
@@ -1,9 +1,43 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100vw;
+    width: 70vw;
     padding-top: 10vh;
     height: 90vh;
 
-    background-color: #f8f8f8;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+
+    .title_background {
+        width: 25rem;
+        height: 3rem;
+        border-radius: 0 3rem 3rem 0;
+        background-color: #ff15e8;
+        position: relative;
+        top: 4.5rem;
+
+        opacity: 0.1;
+    }
+
+    .title {
+        font-family: "PeoplefirstFightingTTF";
+        font-size: 3rem;
+        color: #ff15e8;
+    }
+
+    .subTitle {
+        margin-top: 1rem;
+
+        font-family: "PeoplefirstNeatLoudTTF";
+        font-size: 1.5rem;
+        color: #8f8f8f;
+    }
+
+    .postBox {
+        margin-top: 3vw;
+    }
 `;
